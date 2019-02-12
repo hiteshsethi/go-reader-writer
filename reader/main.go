8ef14ef5eb4498bdec53a3817b8f9ffead1434a5
hiteshsethi <hitesh.28jan@gmail.com>
2019-02-12 16:26:35 +0530

    added multi reader writer test case

--- reader/main.go
+++ reader/main.go
@@ -8,11 +8,16 @@ import (
 
 func main() {
 	for {
-		data, err := ioutil.ReadFile("/data/random.txt")
+		data1, err := ioutil.ReadFile("/data1/random.txt")
 		if err != nil {
 			fmt.Println("Error: ", err)
 		}
-		fmt.Println(string(data))
+		fmt.Println(string(data1))
+		data2, err := ioutil.ReadFile("/data2/random.txt")
+		if err != nil {
+			fmt.Println("Error: ", err)
+		}
+		fmt.Println(string(data2))
 		time.Sleep(1 * time.Second)
 	}
 }
--- writer/main.go
+++ writer/main.go
@@ -7,14 +7,21 @@ import (
 )
 
 func main() {
-	file, err := os.Create("/data/random.txt")
+	file1, err := os.Create("/data1/random.txt")
 	if err != nil {
 		fmt.Println("Error: ", err)
 		os.Exit(1)
 	}
-	defer file.Close()
+	file2, err := os.Create("/data2/random.txt")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	defer file1.Close()
+	defer file2.Close()
 	for {
-		file.WriteString("hello\n")
+		file1.WriteString("hello data 1\n")
+		file2.WriteString("hello data 2\n")
 		fmt.Println("written  hello in file")
 		time.Sleep(5 * time.Second)
 	}
